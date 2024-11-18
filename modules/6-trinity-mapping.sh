fcf09a73fd65ecccd0edb053b95d2ec6c7c2fa2c
Rama Krishnan <104147619+krishnan-Rama@users.noreply.github.com>
2024-11-18 12:01:26 +0000

    Update 6-trinity-mapping.sh

--- modules/6-trinity-mapping.sh
+++ modules/6-trinity-mapping.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=<pipeline>
-#SBATCH --partition=<HPC_partition>       # the requested queue
-#SBATCH --nodes=1              # number of nodes to use
-#SBATCH --tasks-per-node=1     #
-#SBATCH --cpus-per-task=16      #   
-#SBATCH --mem=150000     # in megabytes, unless unit explicitly stated
+#SBATCH --partition=<HPC_partition>       
+#SBATCH --nodes=1              
+#SBATCH --tasks-per-node=1     
+#SBATCH --cpus-per-task=16      
+#SBATCH --mem=150000    
 
 echo "Some Usable Environment Variables:"
 echo "================================="
