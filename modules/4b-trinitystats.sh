634976780ab124c1f895f82930afb52511ec5642
Rama Krishnan <104147619+krishnan-Rama@users.noreply.github.com>
2025-01-19 14:20:36 +0000

    Update 4b-trinitystats.sh

--- modules/4b-trinitystats.sh
+++ modules/4b-trinitystats.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=<pipeline>
+#SBATCH --partition=<HPC_partition>     
+#SBATCH --nodes=1              
+#SBATCH --tasks-per-node=1     
+#SBATCH --cpus-per-task=4      
+#SBATCH --mem=8G     
 
+echo "Some Usable Environment Variables:"
+echo "================================="
+echo "hostname=$(hostname)"
+echo \$SLURM_JOB_ID=${SLURM_JOB_ID}
+echo \$SLURM_NTASKS=${SLURM_NTASKS}
+echo \$SLURM_NTASKS_PER_NODE=${SLURM_NTASKS_PER_NODE}
+echo \$SLURM_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+echo \$SLURM_JOB_CPUS_PER_NODE=${SLURM_JOB_CPUS_PER_NODE}
+echo \$SLURM_MEM_PER_CPU=${SLURM_MEM_PER_CPU}
 
-# Write jobscript to output file (good for reproducability)
 cat $0
 
 # load singularity module
